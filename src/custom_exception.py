9f19336abb696f7db94675244548619580492c3b
Sarvesh Kumar Yadav <105879697+Sarvesh-Yadav-5201@users.noreply.github.com>
2025-06-20 02:38:04 +0530

    commit

--- src/custom_exception.py
+++ src/custom_exception.py
@@ -18,7 +18,7 @@ class CustomException(Exception):
         """
         Returns a detailed error message including the traceback.
         """
-        _ , _ , exc_tb =traceback.sys.exc_info()
+        _ , _ , exc_tb = traceback.sys.exc_info()
         file_name = exc_tb.tb_frame.f_code.co_filename
         line_number = exc_tb.tb_lineno
 
--- utils/common_functions.py
+++ utils/common_functions.py
@@ -4,7 +4,7 @@ import pandas
 from src.logger import get_logger
 from src.custom_exception import CustomException    
 import yaml
-import pandas 
+import pandas  as  pd
 
 ## Creating the instance for logger
 logger = get_logger(__name__)
@@ -13,17 +13,16 @@ logger = get_logger(__name__)
 def read_yaml(file_path):
     try:
         if not os.path.exists(file_path):
-
-            raise FileNotFoundError(f'File Not Found! @ {file_path}')
+            raise FileNotFoundError(f"File is not in the given path")
         
-        with open(file_path, 'r') as yaml_file:
-            config  = yaml.safe_load(yaml_file)
-            logger.info(f"YAML file loaded successfully from {file_path}")
+        with open(file_path,"r") as yaml_file:
+            config = yaml.safe_load(yaml_file)
+            logger.info("Succesfully read the YAML file")
             return config
     
     except Exception as e:
-        logger.error(f"Error occurred while reading YAML file")
-        raise CustomException('Failed to read YAML file.', e)
+        logger.error("Error while reading YAML file")
+        raise CustomException("Failed to read YAMl file" , e)
 
 
 
@@ -36,7 +35,7 @@ def load_data(file_path):
         if not os.path.exists(file_path):
             raise FileNotFoundError(f'File Not Found! @ {file_path}')
 
-        data = pandas.read_csv(file_path)
+        data = pd.read_csv(file_path)
 
         logger.info(f"Data loaded successfully from {file_path}")
         return data
